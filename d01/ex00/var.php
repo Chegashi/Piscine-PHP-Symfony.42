ac37eb5648d22ecfe77dd624e82ab1b29f95fc2e
Chegri <abort@X270.Home>
2020-10-14 00:54:28 +0100

    add day01

--- d01/ex00/var.php
+++ d01/ex00/var.php
@@ -1,10 +1,10 @@
 <?php
     $a = 10;
     $b = '10';
-    $c = 'ten';
+    $c = "ten";
     $d = 10.0;
     echo "a contains : ".$a." and has type : "; echo gettype($a)."\n";
-    echo "b contains : ".$a." and has type : "; echo gettype($b)."\n";
-    echo "c contains : ".$a." and has type : "; echo gettype($c)."\n";
-    echo "d contains : ".$a." and has type : "; echo gettype($d)."\n";
+    echo "b contains : ".$b." and has type : "; echo gettype($b)."\n";
+    echo "c contains : ".$c." and has type : "; echo gettype($c)."\n";
+    echo "d contains : ".$d." and has type : "; echo gettype($d)."\n";
 ?>
